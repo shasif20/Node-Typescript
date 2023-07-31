0aaaf096ab5ea2505b9d6e2351478c2e84561a8c
shasif20 <31159872+shasif20@users.noreply.github.com>
2023-07-31 15:41:53 +0500

    refactoring

--- src/utils/calculateStock.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-module.exports = function (transaction, stockLevel) {
-    if (transaction.type === 'order') {
-        return stockLevel - transaction.qty;
-    }
-    return stockLevel + transaction.qty;
-};
-//# sourceMappingURL=calculateStock.js.map
